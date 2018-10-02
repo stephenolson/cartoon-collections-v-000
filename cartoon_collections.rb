df8a84c3890777adf73d50f753ed2f1daec5259d
Stephen Olson <stephenolson@gmail.com>
2018-10-02 19:19:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
-def roll_call_dwarves(dwarves)
-  i = 0
-  while i < dwarves.length
-    yield(dwarces[i])
-    i += 1
+def roll_call_dwarves(array)
+  i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
   end 
 end
 
